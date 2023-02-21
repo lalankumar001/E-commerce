e2feea90beb963b4552352025b72539149bc4b8e
lalankumar001 <tocsi.org@gmail.com>
2023-02-21 22:17:50 +0530

    cartItemWorking

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import CartProvider from "./store/CartProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-   <BrowserRouter>
-   <App />
-   </BrowserRouter>
-  
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </BrowserRouter>
+);
